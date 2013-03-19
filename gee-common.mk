6d5e786235f93f62a5cef436910e8f4e304de1b5
houst0nn <houstonn@gmail.com>
2013-03-19 18:43:07 -0300

    Properly listed baseband now... was showing up unknown on all aosp about info..
    had previously tried so many.. this is the only one I found that would work..

--- gee-common.mk
+++ gee-common.mk
@@ -143,6 +143,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
+# Baseband properly shown in about info instead of Unknown
+PRODUCT_PROPERTY_OVERRIDES += \
+	gsm.version.baseband=APQ8064/MDM9x15M
+
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072
 
